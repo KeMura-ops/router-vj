91abe625e9b4f425327a2ce3005aff9f531218f6
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-30 22:54:20 +0900

    JS(プログラム)側からURLを切り替える

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h3>Home</h3>
+    <button @click="toUsers">Usersのページへ行く</button>
   </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      toUsers() {
+        // ビュールーターへのアクセス
+        this.$router.push({ path: 'users' }); // push('users');でも良い
+      }
+    }
+  }
+</script>

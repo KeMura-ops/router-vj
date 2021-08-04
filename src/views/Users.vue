8cbd5e020ae67e168669dab3dfc8c324ed6c1ec7
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-04 13:09:49 +0900

    vue-routerにクエリを使用する

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -6,10 +6,10 @@
     <hr>
     <!-- ルートコンポーネントの再利用性を高めるためにはpropsの値で受け取る -->
     <h1>User No. {{ id }}</h1>
-    <!-- v-bindでto属性を動的に表現する -->
-    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
-    <!-- 名前付きルートを使用してリンク先のURLをわかりやすく記述する -->
-    <router-link :to="{ name: 'users-id-profile', params: {id: Number(id) + 1} }">次のユーザー</router-link>
+    <!-- v-bindでto属性を動的に表現する -->              <!-- クエリを直接書いても良い -->
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja'">次のユーザー</router-link>
+    <!-- クエリの指定ができる。(?lang=ja&page=2) -->
+    <router-link :to="{ name: 'users-id-profile', params: {id: Number(id) + 1}, query: { lang: 'ja', page: 2 }}">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>

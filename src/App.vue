939a4a21236aa665b91e84bc652f8725c0b6747a
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-05 13:56:44 +0900

    名前付きビューを使用して、固定レイアウトを作成する

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
-    <nav>
-      <!-- アクティブクラス属性とイグザクト属性を用いてアクティブなリンクとする -->
-      <!-- イグザクトは対象のリンクをアクティブにする -->
-      <router-link to="/" active-class="link--active" exact="" class="link">Home</router-link>
-      <router-link to="/users" active-class="link--active" exact="" class="link">Users</router-link>
-      <!-- コンソール上の表示はaタグになる -->
-    </nav>
     <!-- router-viewコンポーネントでルーティングするコンポーネントを使用 -->
+    <!-- 名前付きルータービューの使用(HeaderUser.vueの使用) -->
+    <router-view name="header"></router-view>
     <router-view></router-view>
   </div>
 </template>
-
-<style scoped>
-  .link {
-    margin-right: 10px;
-  }
-
-  /* リンクがアクティブな際のスタイル付与 */
-  .link--active {
-    font-size: 20px;
-  }
-</style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ import Home from './views/Home.vue';
 import Users from './views/Users.vue';
 import UsersPosts from './views/UsersPosts.vue';
 import UsersProfile from './views/UsersProfile.vue';
+import HeaderHome from './views/HeaderHome.vue';
+import HeaderUser from './views/HeaderUser.vue';
 
 Vue.use(Router) // プラグインの適用
 
@@ -12,10 +14,19 @@ export default new Router({ // ルーティング main.jsにて登録
   // historyモードでURLに「#」が付かなくなる
   // ただし、そのURLに対応したindex.htmlを用意する必要がある
   mode: "history",
-  routes: [{path: '/', component: Home}, 
+  routes: [{path: '/',
+  // 固定レイアウトその1(HeaderHome.vue)
+  components: {
+    default: Home,
+    header:HeaderHome
+  }}, 
     // propsオプションをtrueにする
     { path: '/users/:id',
-      component: Users,
+    // 固定レイアウトその2(HeaderUser.vue)
+      components: {
+        default: Users,
+        header:HeaderUser
+      },
       props: true,
       // ネストされたrouter-viewに表示させるコンポーネントの登録
       children: [

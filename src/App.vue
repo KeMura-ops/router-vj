4f4b290d85f27e175ce82763878fc403fe377fa1
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-08 16:14:22 +0900

    router-viewにトランジションを適用する

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@
     <!-- router-viewコンポーネントでルーティングするコンポーネントを使用 -->
     <!-- 名前付きルータービューの使用(HeaderUser.vueの使用) -->
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
+
+<style scoped>
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+</style>

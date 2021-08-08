738f1d7da9b3d1fbca60e7c9d230bc43e27f86f6
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-08 16:38:49 +0900

    スクロール機能を使用するためhashを付与する

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -2,7 +2,8 @@
   <nav>
     <h2>Home</h2>
     <router-link to="/" active-class="link--active" exact="" class="link">Home</router-link>
-    <router-link to="/users" active-class="link--active" exact="" class="link">Users</router-link>
+    <!-- ブラウザから直接アクセスすることによってスクロールが可能であるが、Vuerouter側の仕様としてそれは出来ない -->
+    <router-link :to="{ name: 'users-id-profile', params: {id: 1}, query: { lang: 'ja', page: 2 }, hash: '#next-user'}" active-class="link--active" exact="" class="link">Users</router-link>
   </nav>
 </template>
 
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -7,10 +7,13 @@
     <!-- ルートコンポーネントの再利用性を高めるためにはpropsの値で受け取る -->
     <h1>User No. {{ id }}</h1>
     <!-- v-bindでto属性を動的に表現する -->              <!-- クエリを直接書いても良い -->
-    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja'">次のユーザー</router-link>
-    <!-- クエリの指定ができる。(?lang=ja&page=2) -->
-    <router-link :to="{ name: 'users-id-profile', params: {id: Number(id) + 1}, query: { lang: 'ja', page: 2 }}">次のユーザー</router-link>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja#next-user'">次のユーザー</router-link>
     <router-view></router-view>
+    <div style="height: 700px"></div>
+    <!-- スクロール機能の使用のためにhashを付与する -->
+    <router-link id="next-user" :to="{ name: 'users-id-profile', params: {id: Number(id) + 1}, query: { lang: 'ja', page: 2 }, hash: '#next-user'}">
+      次のユーザー
+    </router-link>
   </div>
 </template>
 

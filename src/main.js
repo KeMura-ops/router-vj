b1db03ee8c8b15d69ca1db60c1e62dc4f4d7a40b
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-12 13:22:32 +0900

    beforeEachガードの使い方

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router' // ルーターをVue.jsに適用させる
 
 Vue.config.productionTip = false
 
+// ナビゲーションガード(全てのページに遷移する前に特定の処理を行う)
+// ユーザー認証を行っていない場合に、ログインや新規登録フォームに遷移させたい時などに使用する
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach');
+  next();
+})
+
 new Vue({
   router, // router.jsのthis.appを指す
   render: h => h(App),

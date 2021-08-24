565e4121225c0d846996decbf87b58e2740252a3
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-24 22:36:04 +0900

    mapMutationsを用いてコンポーネントから参照する

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -4,21 +4,17 @@
     <router-link to="/" active-class="link--active" exact="" class="link">Home</router-link>
     <!-- ブラウザから直接アクセスすることによってスクロールが可能であるが、Vuerouter側の仕様としてそれは出来ない -->
     <router-link :to="{ name: 'users-id-profile', params: {id: 1}, query: { lang: 'ja', page: 2 }, hash: '#next-user'}" active-class="link--active" exact="" class="link">Users</router-link>
-    <button @click="inc">+1</button>
-    <button @click="dec">-1</button>
+    <button @click="increment(2)">+1</button>
+    <button @click="decrement(2)">-1</button>
   </nav>
 </template>
 
 <script>
+  // mapMutationsを用いてコンポーネントから参照する
+  import { mapMutations } from "vuex";
   export default {
     methods: {
-      // Vuexのプロパティにアクセス
-      inc() {
-        this.$store.commit('increment', 2); // 「commit」でミューテーションにアクセス
-      },
-      dec() {
-        this.$store.commit('decrement', 2);
-      }
+      ...mapMutations(["increment", "decrement"])
     }
   }
 </script>

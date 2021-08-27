70902e0cf813ce6d49500997a1b6341f5fa8f6b0
KeMura-ops <kenti432@yahoo.co.jp>
2021-08-27 13:49:56 +0900

    v-modelを使用した双方向データバインディング

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,32 +4,25 @@
     <button @click="toUsers">Usersのページへ行く</button>
     <p>{{ doubleCount }}</p>
     <p>{{ tripleCount }}</p>
-    <input type="text" :value="message" @input="updateMessage">
+    <input type="text" v-model="message">
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-  // mapGettersのインポート
-  import { mapGetters } from "vuex";
-
   export default {
     computed: {
-      /*
-      スプレッド演算子「...」を使用し、mapGettersをcomputedに結合することで
-      その他のcomputedも記述できる
-      */
-      ...mapGetters(["doubleCount", "tripleCount"]),
-      message() {
-        return this.$store.getters.message;
+      message: {
+        get() {
+          return this.$store.getters.message;
+        },
+        set(value) {
+          this.$store.dispatch("updateMessage", value);
+        }
       }
     },
     methods: {
-      updateMessage(e) {
-        this.$store.dispatch("updateMessage", e.target.value)
-      },
       toUsers() {
-        // Homeからアクセスする
         this.$router.push({
           name: 'users-id-profile',
           params: {id: 1}
